01ea1cc52c84727023f88fcb53d2aab7413d63ea
sojeongsojeong-park <psj90972@naver.com>
2022-07-14 23:34:38 +0900

    feat: 회원가입 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import KakaoLogin from './pages/login/Kakao.js';
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+	return (
+		<div className="App">
+			<KakaoLogin />
+		</div>
+	);
 }
 
 export default App;
