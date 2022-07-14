9a61094d4057ee202ccf9dbde8ae91eff83a6127
[SW2/부산] 윤상진 <newborn18@naver.com>
2022-07-14 21:46:02 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
-}
-
-export default App;
 import React from 'react';
 import { Reset } from 'styled-reset';
 import './App.css';
