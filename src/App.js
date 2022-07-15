9f31456a37d32b897a8dfb4ce210ecb83787ee1e
[SW2/부산]박소정 <psoj90972@gmail.com>
2022-07-15 02:32:58 +0000

    Revert "Merge branch 'feature-login' into 'master'"
    This reverts merge request !1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Reset } from 'styled-reset';
-import Login from './pages/login/Login.jsx';
-import LoginForEmail from './pages/login/LoginForEmail.jsx';
-import Signup from './pages/login/Signup.jsx';
 
 function App() {
-	return (
-		<div className="App">
-			<Reset />
-			<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/loginForEmail" element={<LoginForEmail />} />
-				<Route path="/signup" element={<Signup />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
 }
 
 export default App;
