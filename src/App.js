08e5236cd8bd2d057efcd06d07e3ee81354fe2ba
Hakseong-Lee <83964261+Hakseong-Lee@users.noreply.github.com>
2022-07-15 15:23:37 +0900

    파일생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Writing from './pages/writing/Writing';
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Writing />
     </div>
   );
 }
--- src/pages/writing/Writing.jsx
+++ src/pages/writing/Writing.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
+const WritingHeader = styled.h1`
+	font-size: 1.6rem;
+`;
 const Writing = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return <WritingHeader>부산여행</WritingHeader>;
+};
 
 export default Writing;
