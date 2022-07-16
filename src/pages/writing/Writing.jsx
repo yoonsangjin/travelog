06ac9487780129623534a46bdcc03c40ecd3a581
Hakseong-Lee <83964261+Hakseong-Lee@users.noreply.github.com>
2022-07-16 17:46:45 +0900

    글쓰기 페이지 사이드바 구현

--- src/pages/writing/Writing.jsx
+++ src/pages/writing/Writing.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import WritingSidebar from './WritingSidebar';
+
+const WritingSection = styled.section`
+	width: 100%;
+	height: 95vh;
+	display: flex;
+`;
+const WritingContainer = styled.div`
+  width 70rem;
+  height:100%;
+  display:flex;
+`;
+
 const WritingHeader = styled.h1`
 	font-size: 1.6rem;
 `;
+
 const Writing = () => {
-	return <WritingHeader>부산여행</WritingHeader>;
+	return (
+		<WritingSection>
+			<WritingSidebar />
+			<WritingContainer>
+				<WritingHeader></WritingHeader>
+			</WritingContainer>
+		</WritingSection>
+	);
 };
 
 export default Writing;
