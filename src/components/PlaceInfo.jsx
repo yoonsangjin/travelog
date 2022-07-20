83cd49f92a35e54c17f8f534179b130c762f0299
[SW2/부산] 윤상진 <newborn18@naver.com>
2022-07-20 18:37:34 +0900

    feat : 카테고리별 border color 적용, 즐겨찾기 기능 구현 중

--- src/components/PlaceInfo.jsx
+++ src/components/PlaceInfo.jsx
@@ -10,11 +10,11 @@ function PlaceInfo() {
 
 	function handleStyle(data) {
 		if (data.category_group_code == 'AT4') {
-			return { border: '1px solid rgb(3, 155, 0)' };
+			return { border: '2px double rgb(3, 155, 0)' };
 		} else if (data.category_group_code == 'FD6') {
-			return { border: '1px solid rgb(0, 41, 254)' };
+			return { border: '2px double rgb(0, 41, 254)' };
 		} else if (data.category_group_code == 'CE7') {
-			return { border: '1px solid rgb(224, 88, 54)' };
+			return { border: '2px double rgb(224, 88, 54)' };
 		}
 	}
 
