8efbbe5c77f2504b634ae91eedb4c163d88d6d6e
[SW2/부산] 윤상진 <newborn18@naver.com>
2022-07-24 15:22:13 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !11

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.js'
-import { BrowserRouter } from 'react-router-dom'
-//DnD
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DndProvider backend={HTML5Backend}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </DndProvider>,
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
