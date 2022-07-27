a0016562ea4d2e805d92e353ea4e0dda00a83ec1
sojeongsojeong-park <psj90972@naver.com>
2022-07-27 16:05:37 +0900

    Fix: 라우팅 오류 해결

--- src/index.js
+++ src/index.js
@@ -2,25 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 //DnD
 import { RecoilRoot } from 'recoil';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import Title from './pages/title/Title';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <DndProvider backend={HTML5Backend}>
     <React.StrictMode>
-      <RecoilRoot>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Title />} />
-          </Routes>
+      <BrowserRouter>
+        <RecoilRoot>
           <App />
-        </BrowserRouter>
-      </RecoilRoot>
+        </RecoilRoot>
+      </BrowserRouter>
     </React.StrictMode>
   </DndProvider>,
 );
