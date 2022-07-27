cfd9e47b9f779e2e8317180bd38e631e80a83b6d
sojeongsojeong-park <psj90972@naver.com>
2022-07-27 14:27:22 +0900

    feat: 특정 경로에서 특정 컴포넌트를 띄우지 않기위해 라우트 구조를 수정하였습니다.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.js'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //DnD
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import { RecoilRoot } from 'recoil';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import Title from './pages/title/Title';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <DndProvider backend={HTML5Backend}>
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Title />} />
+          </Routes>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
     </React.StrictMode>
   </DndProvider>,
-)
+);
